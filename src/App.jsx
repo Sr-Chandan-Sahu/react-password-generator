c9de4be225c349c8d6a4ebcaef1c41deea352174
Chandan Sahu <cs154896@gmail.com>
2024-06-26 05:15:26 +0530

    Password Generator

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PasswordGenetor from './components/PasswordGenetor'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <PasswordGenetor/>
+    </>
+  )
+}
+
+export default App
